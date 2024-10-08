61a18bd961d314beedae346c291374fcd45c6691
junhao chen <junhaochen774@gmail.com>
2024-10-08 15:56:29 +0800

    feat(DataPage): accomplish data page as the ancestor page type The class is created for reusing page which is deleted.

--- src/include/storage/page/data_page.h
+++ src/include/storage/page/data_page.h
@@ -5,7 +5,30 @@
 //
 //===-----------------------------------------------------
 
-#ifndef DISTRIBUTION_LSH_SRC_INCLUDE_STORAGE_PAGE_DATA_PAGE_H_
-#define DISTRIBUTION_LSH_SRC_INCLUDE_STORAGE_PAGE_DATA_PAGE_H_
+#pragma once
 
-#endif //DISTRIBUTION_LSH_SRC_INCLUDE_STORAGE_PAGE_DATA_PAGE_H_
+#include <common/config.h>
+
+namespace distribution_lsh {
+
+#define COMMON_DATA_PAGE_HEADER_SIZE 4
+
+/**
+ * @brief the common data page contains the next slot page in the project
+ */
+class DataPage {
+ public:
+  /** Delete all constructor / destructor to ensure memory safety */
+  DataPage() = delete;
+  DataPage(const DataPage &other) = delete;
+  ~DataPage() = delete;
+
+  [[nodiscard]] auto GetNextSlotPageId() const -> page_id_t { return next_slot_page_id_; }
+  void SetNextSlotPageId(page_id_t next_slot_page_id) { next_slot_page_id_ = next_slot_page_id; }
+
+ private:
+  page_id_t next_slot_page_id_{INVALID_PAGE_ID};
+};
+
+
+} // namespace distribution_lsh
