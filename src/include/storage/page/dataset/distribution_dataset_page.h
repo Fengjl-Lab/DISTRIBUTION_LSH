112c10c8a101fcfb8a9f292cca033004f0ab5c24
junhao chen <junhaochen774@gmail.com>
2024-10-08 16:01:29 +0800

    refactor(distribution dataset page): inheritage for resource reused

--- src/include/storage/page/dataset/distribution_dataset_page.h
+++ src/include/storage/page/dataset/distribution_dataset_page.h
@@ -9,14 +9,15 @@
 
 #include <common/config.h>
 #include <storage/page/header_page.h>
+#include <storage/page/data_page.h>
 
 namespace distribution_lsh {
 
-#define DISTRIBUTION_DATASET_PAGE_HEADER_SIZE 13
+#define DISTRIBUTION_DATASET_PAGE_HEADER_SIZE 24
 
-enum class DistributionDatatSetPageType : std::uint8_t {INVALID_DATA_SET_PAGE_TYPE = 0 , DIRECTORY_PAGE, DATA_PAGE};
+enum class DistributionDataSetPageType : std::uint8_t {INVALID_DATA_SET_PAGE_TYPE = 0 , DIRECTORY_PAGE, DATA_PAGE};
 
-class DistributionDataSetPage {
+class DistributionDataSetPage : public DataPage {
   template<class ValueType>
   friend class DistributionDataSetManager;
  public:
@@ -24,16 +25,16 @@ class DistributionDataSetPage {
   DistributionDataSetPage(const DistributionDataSetPage &other) = delete;
   ~DistributionDataSetPage() = delete;
 
-  auto IsDirectoryPage() const -> bool;
-  void SetPageType(DistributionDatatSetPageType page_type);
+  [[nodiscard]] auto IsDirectoryPage() const -> bool;
+  void SetPageType(DistributionDataSetPageType page_type);
 
-  auto GetSize() const -> int;
+  [[nodiscard]] auto GetSize() const -> int;
   void SetSize(int size);
 
-  auto GetMaxSize() const -> int;
+  [[nodiscard]] auto GetMaxSize() const -> int;
   void SetMaxSize(int max_size);
 
-  auto GetNextPageId() const -> page_id_t;
+  [[nodiscard]] auto GetNextPageId() const -> page_id_t;
   void SetNextPageId(page_id_t next_page_id);
 
   void IncreaseSize(int amount);
@@ -41,10 +42,10 @@ class DistributionDataSetPage {
   virtual auto ToString() -> std::string = 0;
 
  private :
-  DistributionDatatSetPageType page_type_;
   int size_;
   int max_size_;
   page_id_t next_page_id_;
+  DistributionDataSetPageType page_type_;
 };
 
 } // namespace distribution_lsh
